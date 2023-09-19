b226688231a1da496b1c61dba523a029dad1e723
boko-marc <marcboko.uriel@gmail.com>
2023-09-19 22:45:03 +0100

    feat:create Game Entity with migrations

---
+++ src/Entity/Game.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\GameRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: GameRepository::class)]
+class Game
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?int $film_id = null;
+
+    #[ORM\Column(type: "integer", options: ["default" => 0])]
+    private ?int $score = 0;
+
+    #[ORM\Column(type: "boolean", options: ["default" => false])]
+    private ?bool $statut = false;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFilmId(): ?int
+    {
+        return $this->film_id;
+    }
+
+    public function setFilmId(?int $film_id): static
+    {
+        $this->film_id = $film_id;
+
+        return $this;
+    }
+
+    public function getScore(): ?int
+    {
+        return $this->score;
+    }
+
+    public function setScore(int $score): static
+    {
+        $this->score = $score;
+
+        return $this;
+    }
+
+    public function isStatut(): ?bool
+    {
+        return $this->statut;
+    }
+
+    public function setStatut(bool $statut): static
+    {
+        $this->statut = $statut;
+
+        return $this;
+    }
+}
